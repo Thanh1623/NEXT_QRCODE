029e2c74abf9f679bb4e4b19a3f4a8b529184fff
Nguyễn Duy Thành <viechoctap6789@gmail.com>
2025-06-09 22:57:12 +0700

    Auto logout when access token expires

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,15 +7,27 @@ const unAuthPaths = ["/login"];
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const isAuth = Boolean(request.cookies.get("accessToken")?.value);
+  const accessToken = request.cookies.get("accessToken")?.value;
+  const refreshToken = request.cookies.get("refreshToken")?.value;
   // chua dang nhap thi khong cho vao private path
-  if (privatePaths.some((path) => pathname.startsWith(path)) && !isAuth) {
+  if (privatePaths.some((path) => pathname.startsWith(path)) && !refreshToken) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
   // dang nhap roi thi khong cho vao login path
-  if (unAuthPaths.some((path) => pathname.startsWith(path)) && isAuth) {
+  if (unAuthPaths.some((path) => pathname.startsWith(path)) && refreshToken) {
     return NextResponse.redirect(new URL("/", request.url));
   }
+  //dang nhap roi nhung access token het han thi logout
+  if (
+    privatePaths.some((path) => pathname.startsWith(path)) &&
+    !accessToken &&
+    refreshToken
+  ) {
+    const url = new URL("/logout", request.url);
+    url.searchParams.set("refreshToken", refreshToken);
+    return NextResponse.redirect(url);
+  }
+
   return NextResponse.next();
 }
 
