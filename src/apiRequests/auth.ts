1facd92cfa27603063d1ab52ec4d0ec614407cf7
Nguyễn Duy Thành <viechoctap6789@gmail.com>
2025-06-10 20:24:09 +0700

    create refresh token route handler

--- src/apiRequests/auth.ts
+++ src/apiRequests/auth.ts
@@ -3,6 +3,8 @@ import {
   LoginBodyType,
   LoginResType,
   LogoutBodyType,
+  RefreshTokenBodyType,
+  RefreshTokenResType,
 } from "@/schemaValidations/auth.schema";
 
 const authApiRequests = {
@@ -26,6 +28,14 @@ const authApiRequests = {
     http.post("/api/auth/logout", null, {
       baseUrl: "",
     }), // client goi den route handle, khong can truyen accessToken va refreshToken vao body, accessToken va refreshToken tu dong gui qua cookie roi
+
+  sRefreshToken: (body: RefreshTokenBodyType) =>
+    http.post<RefreshTokenResType>("/auth/refresh-token", body),
+
+  refreshToken: () =>
+    http.post<RefreshTokenResType>("/api/auth/refresh-token", null, {
+      baseUrl: "",
+    }),
 };
 
 export default authApiRequests;
