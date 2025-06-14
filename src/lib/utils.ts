0d8934ace3ca4bea74612669ba9bacd54cdd4d08
Nguyễn Duy Thành <viechoctap6789@gmail.com>
2025-06-14 17:56:14 +0700

    Long time no access to the website, access token expired

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,8 @@ import { UseFormSetError } from "react-hook-form";
 import { twMerge } from "tailwind-merge";
 import { EntityError } from "./http";
 import { toast } from "@/hooks/use-toast";
+import jwt from "jsonwebtoken";
+import authApiRequests from "@/apiRequests/auth";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -59,5 +61,52 @@ export const setAccessTokenToLocalStorage = (value: string) => {
 export const setRefreshTokenToLocalStorage = (value: string) => {
   if (isBrowser) {
     localStorage.setItem("refreshToken", value);
+  }
+};
+
+export const CheckAndRefreshToken = async (params?: {
+  onError?: () => void;
+  onSuccess?: () => void;
+}) => {
+  // khong nen dua logic lay access token va refresh token ra khoi function `checkAndRefreshToken`
+  // vi vi thoi diem checkAndRefreshToken() duoc goi thi chung ta se co mot access token va refresh token moi
+  // tranh hien tuong bug no lay access token va refresh token tu lan dau roi goi cho cac lan tiep theo
+  const accessToken = getAccessTokenFromLocalStorage();
+  const refreshToken = getRefreshTokenFromLocalStorage();
+
+  // chua dang nhap thi cung khong cho chay.
+  if (!accessToken || !refreshToken) return;
+
+  const decodedAccessToken = jwt.decode(accessToken) as {
+    exp: number;
+    iat: number;
+  };
+  const decodedRefreshToken = jwt.decode(refreshToken) as {
+    exp: number;
+    iat: number;
+  };
+  // thoi diem het han cua token tinh theo epoch time (s)
+  // con khi dung new Date().getTime() thoi diem het han cua token tinh theo epoch time (ms)
+  const now = Math.round(new Date().getTime() / 1000); // epoch time (s)
+  // truong hop refresh token het han thi khong xu ly nua
+  if (decodedRefreshToken.exp <= now) return;
+  // vi du. access token cua chung ta co thoi gian het han la 10s
+  // thi` minh se kiem tra con 1/3 thoi gian (3s) thi minh se cho refresh token lai
+  // Thoi gian con` lai. se dua tren cong thuc: decodedAccessToken.exp - now
+  // exp la thoi gian het han cua access token
+  // thoi gian het han cua access token dua tren cong thuc: decodedAccessToken.exp - decodedAccessToken.iat
+  if (
+    decodedAccessToken.exp - now <
+    (decodedAccessToken.exp - decodedAccessToken.iat) / 3
+  ) {
+    // goi api refresh token
+    try {
+      const res = await authApiRequests.refreshToken();
+      setAccessTokenToLocalStorage(res.payload.data.accessToken);
+      setRefreshTokenToLocalStorage(res.payload.data.refreshToken);
+      params?.onSuccess && params?.onSuccess();
+    } catch (error) {
+      params?.onError && params?.onError();
+    }
   }
 };
