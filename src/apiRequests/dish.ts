75ccf66f946aa36c0c4608fedf3432d9df41ff94
Nguyễn Duy Thành <viechoctap6789@gmail.com>
2025-07-02 21:17:49 +0700

    Fix build bug with useSearchParams and avoid cache when building

--- src/apiRequests/dish.ts
+++ src/apiRequests/dish.ts
@@ -6,6 +6,8 @@ import {
 } from "@/schemaValidations/dish.schema";
 
 const dishApiRequest = {
+   // Nextjs 15 thi`mac dinh fetch se la { cache: 'no-store' } (dynamic rendering page)
+   // Nextjs 14 mac dinh fetch se la { cache: 'force-cache' } (static rendering page)
   list: () => http.get<DishListResType>("dishes"),
   add: (body: CreateDishBodyType) => http.post<DishListResType>("dishes", body),
   getDish: (id: number) => http.get<DishResType>(`dishes/${id}`),
