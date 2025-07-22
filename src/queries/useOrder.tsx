782c3bc92be70b3a7feb5d93332c803967e8ff1c
Nguyễn Duy Thành <viechoctap6789@gmail.com>
2025-07-22 11:41:35 +0700

    Declare order APIs and display data on UI

--- src/queries/useOrder.tsx
+++ src/queries/useOrder.tsx
@@ -1,5 +1,8 @@
 import orderApiRequest from "@/apiRequests/order";
-import { UpdateOrderBodyType } from "@/schemaValidations/order.schema";
+import {
+  GetOrdersQueryParamsType,
+  UpdateOrderBodyType,
+} from "@/schemaValidations/order.schema";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
 export const useUpdateOrderMutation = () => {
@@ -14,9 +17,23 @@ export const useUpdateOrderMutation = () => {
   });
 };
 
-export const useGetOrderListQuery = () => {
+export const useGetOrderListQuery = (queryParams: GetOrdersQueryParamsType) => {
   return useQuery({
-    queryKey: ["orders"],
-    queryFn: orderApiRequest.getOrderList,
+    queryKey: ["orders", queryParams],
+    queryFn: () => orderApiRequest.getOrderList(queryParams),
+  });
+};
+
+export const useGetOrderDetailQuery = ({
+  id,
+  enabled,
+}: {
+  id: number;
+  enabled: boolean;
+}) => {
+  return useQuery({
+    queryFn: () => orderApiRequest.getOrderDetail(id),
+    queryKey: ["orders", id],
+    enabled,
   });
 };
