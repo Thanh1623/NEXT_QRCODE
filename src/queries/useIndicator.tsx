a7887f43d2c897a714325ee6bb2aee4e3cf6a40b
Nguyễn Duy Thành <viechoctap6789@gmail.com>
2025-08-04 21:31:53 +0700

    Code logic dashboard

---
+++ src/queries/useIndicator.tsx
@@ -0,0 +1,12 @@
+import IndicatorApiRequest from "@/apiRequests/indicator";
+import { DashboardIndicatorQueryParamsType } from "@/schemaValidations/indicator.schema";
+import { useQuery } from "@tanstack/react-query";
+
+export const useDashboardIndicator = (
+  queryParams: DashboardIndicatorQueryParamsType
+) => {
+  return useQuery({
+    queryKey: ["dashboardIndicators", queryParams],
+    queryFn: () => IndicatorApiRequest.getDashboardIndicators(queryParams),
+  });
+};
