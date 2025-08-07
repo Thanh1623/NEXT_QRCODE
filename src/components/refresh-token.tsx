49a10b2f46170be74ca0afb7ca6bd22b4e1ce1c9
Nguyễn Duy Thành <viechoctap6789@gmail.com>
2025-08-07 23:31:54 +0700

    Change auto refresh token role using socket.io

--- src/components/refresh-token.tsx
+++ src/components/refresh-token.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import socket from "@/lib/socket";
 import { CheckAndRefreshToken } from "@/lib/utils";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
@@ -18,27 +19,44 @@ export default function RefreshToken() {
     if (UNAUTHENTICATED_PATH.includes(pathname)) return;
     let interval: any = null;
     // phai goi lan dau tien, vi interval se chay sau thoi gian timeout
-    CheckAndRefreshToken({
-      onError: () => {
-        clearInterval(interval);
-        router.push("/login");
-      },
-    });
+    const onRefreshToken = (force?: boolean) =>
+      CheckAndRefreshToken({
+        onError: () => {
+          clearInterval(interval);
+          router.push("/login");
+        },
+        force,
+      });
+    onRefreshToken();
     // timeout interval phai? be hon thoi gian het han cua access token
     // vi du. access token cua chung ta co thoi gian het han la 10s thi` 1s minh se cho check 1 lan
     const TIMEOUT = 1000;
-    interval = setInterval(
-      () =>
-        CheckAndRefreshToken({
-          onError: () => {
-            clearInterval(interval);
-            router.push("/login");
-          },
-        }),
-      TIMEOUT
-    );
+    interval = setInterval(() => onRefreshToken, TIMEOUT);
+    if (socket.connected) {
+      onConnect();
+    }
 
-    return () => clearInterval(interval);
+    function onConnect() {
+      console.log(socket?.id);
+    }
+
+    function onDisconnect() {
+      console.log("disconnect");
+    }
+
+    function onRefreshTokenSocket() {
+      onRefreshToken(true); // force refresh token
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("refresh-token", onRefreshTokenSocket);
+    return () => {
+      clearInterval(interval);
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("refresh-token", onRefreshTokenSocket);
+    };
   }, [pathname, router]);
   return null;
 }
